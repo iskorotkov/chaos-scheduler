14441ee966507f67f88c1420ba818695a34d3e99
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-11-26 19:15:14 +0500

    refactor: remove extra parameter from NewManifestTemplate method

--- pkg/argo/output/templates/manifest.go
+++ pkg/argo/output/templates/manifest.go
@@ -2,15 +2,8 @@ package templates
 
 import "fmt"
 
-const (
-	ActionCreate = Action("create")
-	ActionDelete = Action("delete")
-)
-
-type Action string
-
 type Resource struct {
-	Action   Action `yaml:"action"`
+	Action   string `yaml:"action"`
 	Manifest string `yaml:"manifest"`
 }
 
@@ -19,18 +12,14 @@ type ManifestTemplate struct {
 	Resource Resource `yaml:"resource"`
 }
 
-func NewManifestTemplate(name string, manifest string, action Action) (ManifestTemplate, error) {
+func NewManifestTemplate(name string, manifest string) (ManifestTemplate, error) {
 	if name == "" {
 		return ManifestTemplate{}, fmt.Errorf("can't create template with empty name")
-	}
-
-	if action != ActionCreate && action != ActionDelete {
-		return ManifestTemplate{}, fmt.Errorf("only 'create' and 'delete' actions are supported")
 	}
 
 	if manifest == "" {
 		return ManifestTemplate{}, fmt.Errorf("can't create template with empty manifest")
 	}
 
-	return ManifestTemplate{name, Resource{action, manifest}}, nil
+	return ManifestTemplate{name, Resource{"create", manifest}}, nil
 }
