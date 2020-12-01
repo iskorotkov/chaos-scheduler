b034832cb73461b0c54c404778fd7632cebf5c92
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-12-01 11:02:14 +0500

    feat: print urls of scheduler and Argo server

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/iskorotkov/chaos-scheduler/api"
 	"github.com/iskorotkov/chaos-scheduler/pkg/server"
 	"log"
@@ -15,6 +16,9 @@ func main() {
 
 	cfg := server.ParseConfig()
 	http.HandleFunc("/scenarios", server.WithConfig(api.Scenarios, cfg))
+
+	fmt.Println("Open http://127.0.0.1:8811 to work with scheduler")
+	fmt.Println("Open http://127.0.0.1:2746 to see workflow progress in Argo UI")
 
 	log.Fatal(http.ListenAndServe(":8811", nil))
 }
