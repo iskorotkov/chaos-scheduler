bb3eefe89bdc50e8c7c456f7dbea0fe9d842feb3
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-12-19 00:28:49 +0500

    feat: change scenario generator from round-robin to advanced

--- internal/web/scenarios/workflows.go
+++ internal/web/scenarios/workflows.go
@@ -4,7 +4,6 @@ import (
 	"github.com/iskorotkov/chaos-scheduler/internal/config"
 	"github.com/iskorotkov/chaos-scheduler/pkg/workflows/assemblers"
 	"github.com/iskorotkov/chaos-scheduler/pkg/workflows/assemblers/extensions"
-	"github.com/iskorotkov/chaos-scheduler/pkg/workflows/experiments"
 	"github.com/iskorotkov/chaos-scheduler/pkg/workflows/experiments/container"
 	"github.com/iskorotkov/chaos-scheduler/pkg/workflows/experiments/node"
 	"github.com/iskorotkov/chaos-scheduler/pkg/workflows/experiments/pod"
@@ -30,34 +29,6 @@ func createWorkflowFromForm(r *http.Request, cfg *config.Config, logger *zap.Sug
 }
 
 func generateWorkflow(params form, cfg *config.Config, logger *zap.SugaredLogger) (templates.Workflow, error) {
-	presetList := generator.PresetsList{
-		ContainerPresets: []experiments.ContainerPreset{
-			container.NetworkLatency{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, NetworkLatency: 300},
-			container.NetworkLoss{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, LossPercentage: 100},
-			container.NetworkCorruption{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, CorruptionPercentage: 100},
-			container.NetworkDuplication{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, DuplicationPercentage: 100},
-			container.CPUHog{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, Cores: 1},
-			container.MemoryHog{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, MemoryConsumption: 1000},
-			container.DiskFill{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, FillPercentage: 100},
-		},
-		PodPresets: []experiments.PodPreset{
-			pod.IOStress{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, UtilizationPercentage: 100},
-			pod.Delete{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, Interval: 1, Force: false},
-		},
-		NodePreset: []experiments.NodePreset{
-			node.CPUHog{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, Cores: 2},
-			node.MemoryHog{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, MemoryPercentage: 90},
-			node.IOStress{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, UtilizationPercentage: 100},
-			node.Restart{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS},
-		},
-	}
-
-	extensionsList := extensions.List{
-		ActionExtensions:   nil,
-		StageExtensions:    []extensions.StageExtension{extensions.UseStageMonitor(cfg.StageMonitorImage, cfg.AppNS, cfg.StageInterval, logger.Named("monitor"))},
-		WorkflowExtensions: []extensions.WorkflowExtension{extensions.UseSteps()},
-	}
-
 	seeker, err := targets.NewSeeker(cfg.AppNS, cfg.AppLabel, logger.Named("targets"))
 	if err != nil {
 		logger.Errorw(err.Error(),
@@ -65,13 +36,14 @@ func generateWorkflow(params form, cfg *config.Config, logger *zap.SugaredLogger
 		return templates.Workflow{}, scenarioGeneratorError
 	}
 
-	g := generator.NewRoundRobin(presetList, seeker, logger.Named("generator"))
+	f := failures(cfg)
+	g := generator.NewAdvancedGenerator(f, seeker, logger.Named("generator"))
 	s, err := g.Generate(generator.Params{Stages: params.Stages, Seed: params.Seed, StageDuration: cfg.StageDuration})
 	if err != nil {
 		logger.Errorw(err.Error(),
 			"params", params,
 			"config", cfg,
-			"presets", presetList)
+			"failures", f)
 
 		if err == generator.NonPositiveStagesError || err == generator.TooManyStagesError {
 			return templates.Workflow{}, scenarioParamsError
@@ -80,13 +52,112 @@ func generateWorkflow(params form, cfg *config.Config, logger *zap.SugaredLogger
 		}
 	}
 
-	a := assemblers.NewModularAssembler(extensionsList)
+	ext := assemblerExtensions(cfg, logger)
+	a := assemblers.NewModularAssembler(ext)
 	wf, err := a.Assemble(s)
 	if err != nil {
 		logger.Errorw(err.Error(),
-			"extensions", extensionsList)
+			"extensions", ext)
 		return templates.Workflow{}, scenarioGeneratorError
 	}
 
 	return wf, nil
+}
+
+func assemblerExtensions(cfg *config.Config, logger *zap.SugaredLogger) extensions.List {
+	return extensions.List{
+		ActionExtensions:   nil,
+		StageExtensions:    []extensions.StageExtension{extensions.UseStageMonitor(cfg.StageMonitorImage, cfg.AppNS, cfg.StageInterval, logger.Named("monitor"))},
+		WorkflowExtensions: []extensions.WorkflowExtension{extensions.UseSteps()},
+	}
+}
+
+func failures(cfg *config.Config) []generator.Failure {
+	return []generator.Failure{
+		{
+			Preset:   container.NetworkLatency{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, NetworkLatency: 300},
+			Scale:    generator.ScaleContainer,
+			Severity: generator.SeverityNonCritical,
+		},
+		{
+			Preset:   container.NetworkLatency{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, NetworkLatency: 3000},
+			Scale:    generator.ScaleContainer,
+			Severity: generator.SeverityCritical,
+		},
+		{
+			Preset:   container.NetworkLoss{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, LossPercentage: 10},
+			Scale:    generator.ScaleContainer,
+			Severity: generator.SeverityNonCritical,
+		},
+		{
+			Preset:   container.NetworkLoss{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, LossPercentage: 90},
+			Scale:    generator.ScaleContainer,
+			Severity: generator.SeverityCritical,
+		},
+		{
+			Preset:   container.NetworkCorruption{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, CorruptionPercentage: 10},
+			Scale:    generator.ScaleContainer,
+			Severity: generator.SeverityNonCritical,
+		},
+		{
+			Preset:   container.NetworkCorruption{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, CorruptionPercentage: 90},
+			Scale:    generator.ScaleContainer,
+			Severity: generator.SeverityCritical,
+		},
+		{
+			Preset:   container.NetworkDuplication{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, DuplicationPercentage: 10},
+			Scale:    generator.ScaleContainer,
+			Severity: generator.SeverityNonCritical,
+		},
+		{
+			Preset:   container.NetworkDuplication{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, DuplicationPercentage: 90},
+			Scale:    generator.ScaleContainer,
+			Severity: generator.SeverityCritical,
+		},
+		{
+			Preset:   container.CPUHog{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, Cores: 1},
+			Scale:    generator.ScaleContainer,
+			Severity: generator.SeverityCritical,
+		},
+		{
+			Preset:   container.MemoryHog{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, MemoryConsumption: 1000},
+			Scale:    generator.ScaleContainer,
+			Severity: generator.SeverityCritical,
+		},
+		{
+			Preset:   container.DiskFill{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, FillPercentage: 90},
+			Scale:    generator.ScaleContainer,
+			Severity: generator.SeverityCritical,
+		},
+		{
+			Preset:   pod.IOStress{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, UtilizationPercentage: 90},
+			Scale:    generator.ScalePod,
+			Severity: generator.SeverityCritical,
+		},
+		{
+			Preset:   pod.Delete{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, Interval: 1, Force: false},
+			Scale:    generator.ScalePod,
+			Severity: generator.SeverityLethal,
+		},
+		{
+			Preset:   node.CPUHog{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, Cores: 2},
+			Scale:    generator.ScaleNode,
+			Severity: generator.SeverityCritical,
+		},
+		{
+			Preset:   node.MemoryHog{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, MemoryPercentage: 90},
+			Scale:    generator.ScaleNode,
+			Severity: generator.SeverityCritical,
+		},
+		{
+			Preset:   node.IOStress{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS, UtilizationPercentage: 90},
+			Scale:    generator.ScaleNode,
+			Severity: generator.SeverityCritical,
+		},
+		{
+			Preset:   node.Restart{Namespace: cfg.ChaosNS, AppNamespace: cfg.AppNS},
+			Scale:    generator.ScaleNode,
+			Severity: generator.SeverityLethal,
+		},
+	}
 }
