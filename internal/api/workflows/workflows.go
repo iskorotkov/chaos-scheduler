a0ab265fdbd80339c1dc408525fff8a50a004c5b
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2021-02-05 12:40:51 +0500

    test: add test for parsing form values

--- internal/api/workflows/workflows.go
+++ internal/api/workflows/workflows.go
@@ -1,18 +1,11 @@
 package workflows
 
 import (
-	"errors"
 	"github.com/go-chi/chi"
 	"github.com/iskorotkov/chaos-scheduler/pkg/server"
 	"go.uber.org/zap"
 	"net/http"
 	"strconv"
-)
-
-var (
-	formParseError = errors.New("couldn't parse form data")
-	paramsError    = errors.New("params are invalid")
-	internalError  = errors.New("internal error occurred")
 )
 
 type form struct {
@@ -33,13 +26,13 @@ func Router(logger *zap.SugaredLogger) http.Handler {
 func parseForm(r *http.Request, logger *zap.SugaredLogger) (form, bool) {
 	stages, err := strconv.ParseInt(r.FormValue("stages"), 10, 32)
 	if err != nil {
-		logger.Errorw(err.Error(), "stages", r.FormValue("stages"))
+		logger.Error(err.Error())
 		return form{}, false
 	}
 
 	seed, err := strconv.ParseInt(r.FormValue("seed"), 10, 64)
 	if err != nil {
-		logger.Errorw(err.Error(), "seed", r.FormValue("seed"))
+		logger.Error(err.Error())
 		return form{}, false
 	}
 
---
+++ internal/api/workflows/workflows_test.go
@@ -0,0 +1,72 @@
+package workflows
+
+import (
+	"go.uber.org/zap"
+	"math"
+	"math/rand"
+	"net/http/httptest"
+	"net/url"
+	"strconv"
+	"testing"
+	"testing/quick"
+)
+
+func Test_parseForm(t *testing.T) {
+	t.Parallel()
+
+	r := rand.New(rand.NewSource(0))
+	f := func() bool {
+		request := httptest.NewRequest("GET", "/url", nil)
+
+		seed := rand.Int63() - math.MaxInt64/2
+		stages := -10 + rand.Intn(120)
+
+		variant := rand.Intn(10)
+		invalidVariants := 3
+		switch variant {
+		case 0:
+			request.PostForm = url.Values{}
+		case 1:
+			request.PostForm = url.Values{
+				"stages": {strconv.FormatInt(int64(stages), 10)},
+			}
+		case 2:
+			request.PostForm = url.Values{
+				"seed": {strconv.FormatInt(seed, 10)},
+			}
+		default:
+			request.PostForm = url.Values{
+				"seed":   {strconv.FormatInt(seed, 10)},
+				"stages": {strconv.FormatInt(int64(stages), 10)},
+			}
+		}
+
+		form, ok := parseForm(request, zap.NewNop().Sugar())
+		if !ok {
+			if variant < invalidVariants {
+				t.Log("not all values were provided")
+				return true
+			} else {
+				t.Log("couldn't parse form")
+				return false
+			}
+		}
+
+		if variant < invalidVariants {
+			t.Log("must return error on invalid form")
+			return false
+		}
+
+		if form.Stages != stages || form.Seed != seed {
+			t.Log("stages and seed must be equal to provided values")
+			return false
+		}
+
+		t.Log("succeeded")
+		return true
+	}
+
+	if err := quick.Check(f, &quick.Config{Rand: r}); err != nil {
+		t.Error(err)
+	}
+}
